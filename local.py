1346a10765392a1ac8e8d87d9915e9aea2ee446f
David Galambos <31393112+galambosd@users.noreply.github.com>
2018-01-21 08:44:26 -0600

    creating local alignment

---
+++ local.py
@@ -0,0 +1,18 @@
+from Bio import SeqIO
+
+#get the fasta files and the scoring files
+
+# parse the fasta file
+# parse the scoring file -- ignore the gap extend
+
+
+# make a class for the table with an init method (hard-coding inits for local/global could be easier)
+# 
+# set up a table
+# initialize it
+# fill out the values
+# keep updating the max value
+# get max value -- store 'hangover' part of alignment
+# traceback from max value until you get to 0
+# get the 'hangover' parts of v and w from the beginning and end
+# print the alignment and the max value
